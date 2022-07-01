8b7cabd53b2fcfb085f63a148a2a6bc8e06769b5
Luz Edith <lizth93@hotmail.com>
2022-07-01 12:08:26 -0500

    Added validation input on the alert

--- controllers/alert.js
+++ controllers/alert.js
@@ -2,15 +2,17 @@ import { container } from "../views/renderCard";
 import { clear } from "../views/renderCard";
 
 export function handlerClickOnSendCard() {
-  const btnEnviar = document.querySelector(".btn");
-  if (!btnEnviar) {
+  const btnSend = document.querySelector(".btn");
+  if (!btnSend) {
     return;
   }
 
-  btnEnviar.addEventListener("click", alert);
+  btnSend.addEventListener("click", alert);
 }
 
+
 function alert() {
+  console.log("executing alert")
   const root = container();
   const markup = `
     <div class="card">
@@ -19,9 +21,10 @@ function alert() {
         Escribe el nombre de la persona a quien quieres enviar la tarjeta:
       </p>
       <form>
-        <input class="input-name" type="text" />
-        <button type="button" class="btn">CANCELAR</button>
-        <button type="submit" class="btn">CONTINUAR</button>
+        <input class="input-name" type="text" required/>
+        <div class="show-error">&nbsp;</div>       
+        <button type="button" class="btn btn-cancel">CANCELAR</button>
+        <button type="submit" class="btn btn-continue">CONTINUAR</button>
       </form>
   </div>`;
 
@@ -30,4 +33,73 @@ function alert() {
   htmlObject.innerHTML = markup;
 
   root.insertAdjacentElement("beforeend", htmlObject);
+
+  handlerClickSubmit()
 }
+
+
+function handlerClickSubmit() {
+  handlerFocusInput()
+  const btnContinue = document.querySelector(".btn-continue")
+  btnContinue.removeEventListener("click", validationInputSubmit)
+  btnContinue.addEventListener("click",validationInputSubmit)
+}
+
+function handlerFocusInput(){
+  const input = getInput()
+
+  input.removeEventListener("focus", isFocus)
+  input.addEventListener("focus", isFocus)
+
+  input.removeEventListener("blur", isBlur)
+  input.addEventListener("blur", isBlur)
+}
+
+function isFocus(){
+  message=""
+  renderError()
+ 
+}
+
+function isBlur(){
+  const name = getInput().value
+  if(name.trim()==="") {
+    message="Por favor ingrese un nombre"
+    renderError()
+  }
+  
+}
+
+function getInput(){
+  return  document.querySelector(".input-name")
+}
+
+
+let message = ""
+
+function validationInputSubmit (e){
+  e.preventDefault()
+
+  const name =  document.querySelector(".input-name").value
+
+  
+  if (name.trim()===""){
+    message="Por favor ingrese un nombre"
+    renderError()
+  } 
+
+}
+
+function renderError(){
+  const root = document.querySelector(".show-error")
+  
+  const markup = `
+  <label class="message">${message}</label></br>`
+
+  root.innerHTML = "";
+  const htmlObject = document.createElement("div");
+  htmlObject.innerHTML = markup;
+
+  root.insertAdjacentElement("beforeend", htmlObject);
+}
+
